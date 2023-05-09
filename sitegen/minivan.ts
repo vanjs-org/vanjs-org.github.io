f893890fbbd008f57c27f33137fe165f8f5c89d7
Tao Xin <tao@vanjs.org>
2023-05-09 09:38:22 -0700

    Revise website

--- sitegen/minivan.ts
+++ sitegen/minivan.ts
@@ -4,7 +4,7 @@ import { HTMLDocument } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm
 
 export default (doc: HTMLDocument) => {
   const {tags} = van.vanWithDoc(doc)
-  const {div, p, path, svg, table, tbody, th, thead, tr} = tags
+  const {a, div, p, path, svg, table, tbody, th, thead, tr} = tags
 
   const {BI, Download, DownloadRow, H1, H2, H3, Html, Js, Link, MiniVan, Symbol, Ts, VanJS} = common(doc)
 
@@ -160,6 +160,18 @@ await serve(req => new Response(
         path({"d": "M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"}),
       ),
       Link("github.com/vanjs-org/mini-van", "https://github.com/vanjs-org/mini-van"),
+    ),
+    H2("Support & Feedback"),
+    p(
+      a({class: "github-button", href: "https://github.com/vanjs-org/mini-van", "data-icon": "octicon-star", "aria-label": "Star vanjs-org/van on GitHub"},
+        "Star",
+      ), " ",
+      a({class: "github-button", href: "https://github.com/vanjs-org/mini-van/subscription", "data-icon": "octicon-eye", "aria-label": "Watch vanjs-org/van on GitHub"},
+        "Watch",
+      ), " ",
+      a({class: "github-button", href: "https://github.com/vanjs-org/mini-van/issues", "data-icon": "octicon-issue-opened", "aria-label": "Issue vanjs-org/van on GitHub"},
+        "Issue",
+      ),
     ),
   )
 }
