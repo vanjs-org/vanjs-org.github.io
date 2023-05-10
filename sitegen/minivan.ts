509769d5bb522473793392b30d249b368787d4b7
Tao Xin <tao@vanjs.org>
2023-05-09 18:44:59 -0700

    Update page for /minivan

--- sitegen/minivan.ts
+++ sitegen/minivan.ts
@@ -6,7 +6,7 @@ export default (doc: HTMLDocument) => {
   const {tags} = van.vanWithDoc(doc)
   const {a, div, p, path, svg, table, tbody, th, thead, tr} = tags
 
-  const {BI, Download, DownloadRow, H1, H2, H3, Html, Js, Link, MiniVan, Symbol, Ts, VanJS} = common(doc)
+  const {BI, Download, DownloadRow, H1, H2, H3, Html, Js, Link, MiniVan, Symbol, SymLink, Ts, VanJS} = common(doc)
 
   const version = Deno.readTextFileSync("code/mini-van.version")
 
@@ -43,10 +43,8 @@ export default (doc: HTMLDocument) => {
   )
 
   return div({id: "content"},
-    H1(MiniVan(), ": A Slimmed-down Version of ", VanJS(), " for Client-side/Server-side DOM Generation"),
-    p(MiniVan(), " is the slimmed-down version of ", Link(VanJS(), "/"), " by stripping out the state and state binding functionalities. ", MiniVan(), " provides the functionalities of DOM composition and manipulation. Compared to ", VanJS(), ", ", MiniVan(), " further reduces the bundle size to 0.5kB and can be used on the server-side as a ", Link("template engine", "https://en.wikipedia.org/wiki/Web_template_system"), "."),
-    p(MiniVan(), " is part of larger ", Link(VanJS(), "/"), " project, which aims to provide an ", BI("ultra-lightweight"), ", ", BI("zero-dependency"), ", and ", BI("unopinionated"), " Reactive UI framework based on pure vanilla JavaScript and DOM."),
-    p("The ", Symbol("Hello World"), " program shown in ", VanJS(), " ", Link("Home", "/"), " page also works with ", MiniVan(), ":"),
+    H1(MiniVan(), ": A Minimalist Template Engine for DOM Generation. Working for both Client-side and Server-side Rendering"),
+    p(MiniVan(), " is an ", BI("ultra-lightweight"), " template engine for DOM composition and manipulation. With only 0.5kB in the minized bundle size, ", MiniVan(), " enables you to build comprehensive UI with elegant and expressive vanilla JavaScript code:"),
     Js(`// Reusable components can be just pure vanilla JavaScript functions.
 // Here we capitalize the first letter to follow React conventions.
 const Hello = () => div(
@@ -66,6 +64,8 @@ van.add(document.body, Hello())
       "data-prefix": "const {a, div, li, p, ul} = van.tags",
       "data-details": "demo-mini-van.details",
     }),
+    p("You can convert any HTML snippet into ", MiniVan(), " code with our online ", Link("converter", "/convert"), "."),
+    p(MiniVan(), " is the slimmed-down version of ", Link(VanJS(), "/"), ", which aims to provide an ", BI("ultra-lightweight"), ", ", BI("zero-dependency"), ", and ", BI("unopinionated"), " Reactive UI framework based on pure vanilla JavaScript and DOM. Compared to ", VanJS(), ", ", MiniVan(), " further reduces the bundle size to 0.5kB and can be used on the server-side as a ", Link("template engine", "https://en.wikipedia.org/wiki/Web_template_system"), "."),
     H2("Getting Started on the Client-Side"),
     p("To get started with ", MiniVan(), ", download the latest version ", Download(`mini-van-${version}.min.js`), " and add the line below to your script:"),
     Js(`import van from "./mini-van-${version}.min.js"`),
@@ -110,11 +110,11 @@ await serve(req => new Response(
   },
 ), {port})
 `),
-    p("As illustrated in the example, ", Symbol("render"), " method can be called on the object returned from the ", Link("tag function", "/tutorial#api-tags"), " to generate a ", Symbol("string"), " that can be   used for serving."),
+    p("As illustrated in the example, ", Symbol("render"), " method can be called on the object returned from the ", SymLink("tag function", "/tutorial#api-tags"), " to generate a ", Symbol("string"), " that can be   used for serving."),
     p(Symbol("van.html"), " is a helper function defined in ", Symbol("van-plate.js"), " that is equivalent to:",
     Js(`(...args) => "<!DOCTYPE html>" + tags.html(...args).render()`)),
     H3(Symbol("mini-van"), " mode"),
-    p("The behavior in ", Symbol("mini-van"), " mode is similar to the behavior in browser context. i.e.: DOM objects will be created by ", Link("tag functions", "/tutorial#api-tags"), ". As Deno doesn't have the built-in support for DOM objects, you need to provide a 3rd-party ", Symbol("Document"), " object before integrating with ", MiniVan(), " in this mode."),
+    p("The behavior in ", Symbol("mini-van"), " mode is similar to the behavior in browser context. i.e.: DOM objects will be created by ", SymLink("tag functions", "/tutorial#api-tags"), ". As Deno doesn't have the built-in support for DOM objects, you need to provide a 3rd-party ", Symbol("Document"), " object before integrating with ", MiniVan(), " in this mode."),
     p("There are multiple 3rd-party options for the ", Symbol("Document"), " object. In the example below, we will demonstrate the integration with the help of ", Link("deno-dom", "https://deno.com/manual@v1.28.1/advanced/jsx_dom/deno_dom"), ":"),
     Ts(`import { serve } from "https://deno.land/std@0.184.0/http/server.ts"
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts"
