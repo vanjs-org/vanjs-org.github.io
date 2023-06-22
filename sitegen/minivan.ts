aaacaf2e80677bc3ec507401971ea04e1464c92a
Tao Xin <tao@vanjs.org>
2023-06-22 14:21:23 -0700

    Revise

--- sitegen/minivan.ts
+++ sitegen/minivan.ts
@@ -240,7 +240,7 @@ await serve(req => new Response(
     p("🙏 ", VanJS(), " aims to build a better world by reducing the entry barrier for UI programming, with no intention or plan on commercialization whatsoever. If you find ", VanJS(), " interesting, or could be useful for you some day, please consider starring the project on ", Link("GitHub", "https://github.com/vanjs-org/mini-van"), ". It takes just a few seconds but your support means the world to us and helps spread ", VanJS(), " to a wider audience."),
     p("We're looking for the 1.0 milestone (commitment to API stability) soon, your precious feedback will be greatly appreciated. You can submit your feedback by creating issues with the link below:"),
     p(
-      a({class: "github-button", href: "https://github.com/vanjs-org/mini-van", "data-icon": "octicon-star", "aria-label": "Star vanjs-org/van on GitHub"},
+      a({class: "github-button", href: "https://github.com/vanjs-org/mini-van", "data-icon": "octicon-star", "data-show-count": true, "aria-label": "Star vanjs-org/van on GitHub"},
         "Star",
       ), " ",
       a({class: "github-button", href: "https://github.com/vanjs-org/mini-van/subscription", "data-icon": "octicon-eye", "aria-label": "Watch vanjs-org/van on GitHub"},
@@ -248,6 +248,9 @@ await serve(req => new Response(
       ), " ",
       a({class: "github-button", href: "https://github.com/vanjs-org/mini-van/issues", "data-icon": "octicon-issue-opened", "aria-label": "Issue vanjs-org/van on GitHub"},
         "Issue",
+      ), " ",
+      a({class: "github-button", href: "https://github.com/vanjs-org", "aria-label": "Follow @vanjs-org on GitHub"},
+        "Follow @vanjs-org",
       ),
     ),
     p("Contact us: ", Link("tao@vanjs.org", "mailto:tao@vanjs.org")),
