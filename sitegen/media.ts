a59371b9d35be1ca2a35879db28d2a81a23995a0
Tao Xin <tao@vanjs.org>
2023-08-07 22:08:07 -0700

    Add more into "Media Mentions" page

--- sitegen/media.ts
+++ sitegen/media.ts
@@ -27,6 +27,7 @@ export default (doc: HTMLDocument) => {
       li(Url("https://changelog.com/jsparty/277"), " (podcasts)"),
       li(Url("https://javascriptweekly.com/issues/650")),
       li(Url("https://javascriptweekly.com/issues/639")),
+      li(Url("https://bytes.dev/archives/211")),
       li(Url("https://bytes.dev/archives/190")),
       li(Url("https://echojs.com/news/41772")),
       li(Url("https://wweb.dev/weekly/150")),
