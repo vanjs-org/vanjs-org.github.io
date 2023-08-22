da72a29a528ff437990bb28a3fbf3a014dcb627c
Tao Xin <tao@vanjs.org>
2023-08-22 08:37:36 -0700

    Simplify Keyboard Event Inspector App

--- demo.js
+++ demo.js
@@ -210,29 +210,15 @@ const {a, b, button, div, i, input, label, li, p, pre, span, strike, table, tbod
   const Value = text => span({class: "value"}, text)
 
   const Inspector = () => {
-    const keyStates = {
-      key: van.state(""),
-      code: van.state(""),
-      which: van.state(""),
-      keyCode: van.state(""),
-      ctrlKey: van.state(false),
-      metaKey: van.state(false),
-      altKey: van.state(false),
-      shiftKey: van.state(false),
-    }
+    const keyEvent = van.state(new KeyboardEvent("keydown"))
 
-    const Result = prop => span(Label(prop + ": "), Value(keyStates[prop]))
-
-    const onkeydown = e => {
-      e.preventDefault()
-      Object.entries(keyStates).forEach(([k, v]) => v.val = e[k])
-    }
+    const Result = prop => span(Label(prop + ": "), Value(keyEvent.val[prop]))
 
     return div(
-      div(input({placeholder: "Focus here and press keys…", onkeydown,
-        style: "width: 260px"})),
-      div(Result("key"), Result("code"), Result("which"), Result("keyCode")),
-      div(Result("ctrlKey"), Result("metaKey"), Result("altKey"), Result("shiftKey")),
+      div(input({placeholder: "Focus here and press keys…", style: "width: 260px",
+        onkeydown: e => (e.preventDefault(), keyEvent.val = e)})),
+      () => div(Result("key"), Result("code"), Result("which"), Result("keyCode")),
+      () => div(Result("ctrlKey"), Result("metaKey"), Result("altKey"), Result("shiftKey")),
     )
   }
 
