070a4ceb539f1db3a148f5001964cdb423ff601f
Tao Xin <tao@vanjs.org>
2023-09-12 16:57:44 -0700

    Add information about MD and HTML to VanJS code converter

--- sitegen/convert.ts
+++ sitegen/convert.ts
@@ -3,11 +3,12 @@ import common from "./common.ts"
 import { HTMLDocument } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts"
 
 export default (doc: HTMLDocument) => {
-  const {tags: {div, p}} = van.vanWithDoc(doc)
-  const {H1} = common(doc)
+  const {tags: {div, i, p}} = van.vanWithDoc(doc)
+  const {H1, Link, VanJS} = common(doc)
 
   return div({id: "content"},
     H1("Convert HTML Snippet to 🍦VanJS Code"),
+    i("The library version of the converter with the support of converting MD and HTML snippets to ", VanJS(), " code is ", Link("working in progress", "https://github.com/vanjs-org/van/tree/main/converter"), "."),
     p({id: "converter"}),
   )
 }
