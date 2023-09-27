e7186b2d3e5d85957290fcb8e0e5a8dc20643c8f
Tao Xin <tao@vanjs.org>
2023-09-27 09:34:17 -0700

    Fix typo

--- sitegen/home.ts
+++ sitegen/home.ts
@@ -38,7 +38,7 @@ export default (doc: HTMLDocument) => {
     H1(VanJS(), ": A 0.9kB Grab 'n Go Reactive UI Framework without React/JSX"),
     p("📣 ", Link(VanJS(), " 1.0.0 is here →", "https://github.com/vanjs-org/van/discussions/72")),
     blockquote(i("Enable everyone to build useful UI apps with a few lines of code, anywhere, any time, on any device.")),
-    p((VanJS()), " (", Link("abbreviated ", b("Van"), "illa ", b("J"), "ava", b("S"), "ript", "/about#name"), ") is an ", BI("ultra-lightweight"), ", ", BI("zero-dependency"), ", and ", BI("unopinionated"), " Reactive UI framework based on pure vanilla JavaScript and DOM. Programming with ", VanJS(), " feels like building React apps in a ", Link("scripting language", "/about#story"), ", without JSX", ". Check-out the ", Symbol("Hello World"), " code below:"),
+    p((VanJS()), " (", Link("abbreviated ", b("Van"), "illa ", b("J"), "ava", b("S"), "cript", "/about#name"), ") is an ", BI("ultra-lightweight"), ", ", BI("zero-dependency"), ", and ", BI("unopinionated"), " Reactive UI framework based on pure vanilla JavaScript and DOM. Programming with ", VanJS(), " feels like building React apps in a ", Link("scripting language", "/about#story"), ", without JSX", ". Check-out the ", Symbol("Hello World"), " code below:"),
     Js(`// Reusable components can be just pure vanilla JavaScript functions.
 // Here we capitalize the first letter to follow React conventions.
 const Hello = () => div(
