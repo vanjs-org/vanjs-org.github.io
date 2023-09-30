ea83c5762a2c7af4b756372d07a592c33a4f66dc
Tao Xin <tao@vanjs.org>
2023-09-30 00:15:41 -0700

    Rename script files to align in naming convention

--- render.sh
+++ render.sh
@@ -1,3 +1,3 @@
 npx deno run --allow-read --allow-write sitegen/render.ts
-npx deno run --allow-read --allow-write sitegen/genFiddleLinks.ts
-npx deno run --allow-read --allow-write sitegen/copyToIndexFiles.ts
+npx deno run --allow-read --allow-write sitegen/gen-fiddle-links.ts
+npx deno run --allow-read --allow-write sitegen/copy-to-index-files.ts
