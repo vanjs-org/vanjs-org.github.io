529ab8d872b9838cf0b4436fd77b803473fae34f
Tao Xin <tao@vanjs.org>
2024-02-05 10:26:25 -0800

    Change the twitter icon to X

--- sitegen/home.ts
+++ sitegen/home.ts
@@ -3,7 +3,7 @@ import common from "./common.ts"
 import { HTMLDocument } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts"
 
 export default (doc: HTMLDocument) => {
-  const {tags: {a, b, blockquote, div, g, i, img, input, label, li, p, path, span, svg, table, tbody, td, th, thead, title, tr, ul}} = van.vanWithDoc(doc)
+  const {tags: {a, b, blockquote, clipPath, defs, div, g, i, img, input, label, li, p, path, rect, span, svg, table, tbody, td, th, thead, title, tr, ul}} = van.vanWithDoc(doc)
   const {BI, Demo, H1, H2, H3, Js, Link, MiniVan, Quote, Symbol, VanJS, VanUI, VanX} = common(doc)
 
   const mailIcon = svg({viewBox: "0 0 16 16", version: "1.1", width: 16, height: 16, "aria-hidden": true},
@@ -19,11 +19,18 @@ export default (doc: HTMLDocument) => {
     ),
   )
 
-  const twitterIcon = svg({xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 273.5 222.3", role: "img", "aria-labelledby": "cidgm4oxr0wshcfdknknay597vhlet4", height: 16, width: 16},
-    title({id: "cidgm4oxr0wshcfdknknay597vhlet4"},
-      "Twitter",
+  const xIcon = svg({xmlns: "http://www.w3.org/2000/svg", width: "16", height: "16", viewBox: "0 0 16 16", fill: "none", role: "img", "aria-labelledby": "a7if5hb4iar7vvxwyraz9fhyh4t2zeon", class: "octicon"},
+    title({id: "a7if5hb4iar7vvxwyraz9fhyh4t2zeon"},
+      "X",
     ),
-    path({"d": "M273.5 26.3a109.77 109.77 0 0 1-32.2 8.8 56.07 56.07 0 0 0 24.7-31 113.39 113.39 0 0 1-35.7 13.6 56.1 56.1 0 0 0-97 38.4 54 54 0 0 0 1.5 12.8A159.68 159.68 0 0 1 19.1 10.3a56.12 56.12 0 0 0 17.4 74.9 56.06 56.06 0 0 1-25.4-7v.7a56.11 56.11 0 0 0 45 55 55.65 55.65 0 0 1-14.8 2 62.39 62.39 0 0 1-10.6-1 56.24 56.24 0 0 0 52.4 39 112.87 112.87 0 0 1-69.7 24 119 119 0 0 1-13.4-.8 158.83 158.83 0 0 0 86 25.2c103.2 0 159.6-85.5 159.6-159.6 0-2.4-.1-4.9-.2-7.3a114.25 114.25 0 0 0 28.1-29.1", fill: "currentColor"}),
+    g({"clip-path": "url(#clip0_1668_3024)"},
+      path({"d": "M9.52217 6.77143L15.4785 0H14.0671L8.89516 5.87954L4.76437 0H0L6.24656 8.8909L0 15.9918H1.41155L6.87321 9.78279L11.2356 15.9918H16L9.52183 6.77143H9.52217ZM7.58887 8.96923L6.95596 8.0839L1.92015 1.03921H4.0882L8.15216 6.7245L8.78507 7.60983L14.0677 14.9998H11.8997L7.58887 8.96957V8.96923Z", fill: "currentColor"}),
+    ),
+    defs(
+      clipPath({id: "clip0_1668_3024"},
+        rect({width: "16", height: "16", fill: "white"}),
+      ),
+    ),
   )
 
   const GithubIcon = ({style}: {style?: string}) => svg({...(style ? {style} : {}), height: 16, "aria-hidden": true, viewBox: "0 0 16 16", version: "1.1", width: 16, "data-view-component": true},
@@ -189,7 +196,7 @@ van.add(document.body, Counter())
     ),
     blockquote(i("In the name of ", b("Van"), "illa of the House ", b("J"), "ava", b("S"), "cript, ", Link("the First of its name", "/about#name"), ", Smallest Reactive UI Framework, 1.0kB JSX-free Grab 'n Go Library, ", Link("Scripting Language", "https://vanjs.org/about#story"), " for GUI, ", Link("GPT-Empowered", "https://chat.openai.com/g/g-7tcSHUu27-vanjs-app-builder"), " Toolkit, by the word of Tao of the House Xin, Founder and Maintainer of ", VanJS(), ", I do hereby grant you the permission of ", VanJS(), " under ", Link("MIT License", "https://github.com/vanjs-org/van/blob/main/LICENSE"), ".")),
     p({class: "contact"},
-      "Contact us: ", twitterIcon, Link("@taoxin", "https://twitter.com/intent/follow?region=follow_link&screen_name=taoxin"), " / ", GithubIcon({}), Link("Tao Xin", "https://github.com/Tao-VanJS"), " / ", mailIcon, Link("tao@vanjs.org", "mailto:tao@vanjs.org"), " / ", linkedInIcon, Link("Tao Xin", "https://www.linkedin.com/in/tao-xin-64234920/"),
+      "Contact us: ", xIcon, Link("@taoxin", "https://x.com/intent/follow?region=follow_link&screen_name=taoxin"), " / ", GithubIcon({}), Link("Tao Xin", "https://github.com/Tao-VanJS"), " / ", mailIcon, Link("tao@vanjs.org", "mailto:tao@vanjs.org"), " / ", linkedInIcon, Link("Tao Xin", "https://www.linkedin.com/in/tao-xin-64234920/"),
     ),
   )
 }
