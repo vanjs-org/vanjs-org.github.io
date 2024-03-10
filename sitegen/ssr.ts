07d85646848ba84184c638e516e4db668a0b305d
Tao Xin <tao@vanjs.org>
2024-03-10 15:35:55 -0700

    Update VanJS's version number to 1.5.0 and update VanJS' bundle size

--- sitegen/ssr.ts
+++ sitegen/ssr.ts
@@ -56,7 +56,7 @@ export default (doc: HTMLDocument) => {
     "finalhandler": "^1.2.0",
     "mini-van-plate": "^0.5.6",
     "serve-static": "^1.15.0",
-    "vanjs-core": "^1.4.1"
+    "vanjs-core": "^1.5.0"
   }
 `),
     ul(
