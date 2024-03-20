8bb92d680485b3cf2ce4257ba64e4f1c7f5cf7d9
Tao Xin <tao@vanjs.org>
2024-03-20 08:43:40 -0700

    Add notes about putting calculated fields separately

--- jsfiddle/tutorial/list/demo.js
+++
@@ -1,5 +0,0 @@
-const {li, ul} = van.tags
-
-const List = ({items}) => ul(items.map(it => li(it)))
-
-van.add(document.body, List({items: ["Item 1", "Item 2", "Item 3"]}))
--- jsfiddle/tutorial/onclick/demo.js
+++
@@ -1,3 +0,0 @@
-const {button} = van.tags
-
-van.add(document.body, button({onclick: () => alert("Hello from 🍦VanJS")}, "Hello"))
