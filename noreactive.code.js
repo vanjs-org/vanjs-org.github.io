005cd5dbe941b2d9941e11de02df8c0f5102ddd9
Tao Xin <tao@vanjs.org>
2024-04-26 21:21:15 -0700

    Adjust the example of vanX.noreactive

--- noreactive.code.js
+++ noreactive.code.js
@@ -3,4 +3,7 @@ const data = vanX.reactive([
   vanX.noreactive(new ArrayBuffer(16)),
 ])
 
-const App = () => vanX.list(div, data, v => div(v.val.byteLength))
+const App = () => div(
+  vanX.list(div, data, v => div(v.val.byteLength)),
+  div(button({onclick: () => data.push(vanX.noreactive(new ArrayBuffer(24)))}, "Push")),
+)
