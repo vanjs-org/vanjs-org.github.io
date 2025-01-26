73deb676a2a0e0970f4330e39b9213fc2aa8af59
Tao Xin <tao@vanjs.org>
2025-01-25 22:46:02 -0800

    Upgrade Mini-Van to 0.6.2

--- sitegen/ssr.ts
+++ sitegen/ssr.ts
@@ -57,7 +57,7 @@ export default (doc: HTMLDocument) => {
     p("Dependencies are declared in ", SymLink("package.json", codeUrlBase + "/package.json"), " file:"),
     Json(`  "dependencies": {
     "finalhandler": "^1.2.0",
-    "mini-van-plate": "^0.6.1",
+    "mini-van-plate": "^0.6.2",
     "serve-static": "^1.15.0",
     "vanjs-core": "^1.5.3"
   }
